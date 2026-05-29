edcc65138b901b2f385e0851f0d13c652d4f517f
videx <45696129+odejinmi@users.noreply.github.com>
2026-05-30 00:13:50 +0100

    updated

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.konn3ct.konn3ctsdk_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
